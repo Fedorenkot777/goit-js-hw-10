a6409fe0cb6cb0f71855393578c85fd59d164a92
Fedorenkot777 <155662260+Fedorenkot777@users.noreply.github.com>
2024-05-20 18:04:37 +0200

    edit333

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,16 +1,13 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
 
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+
 
 const startBtn = document.querySelector("start-button")
 let userSelectedDate = null;
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date,
-    minuteIncrement: 1,
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date,
+  minuteIncrement: 1,
     onClose(selectedDates) {
         console.log(selectedDates[0]);
         if (userSelectedDate <= new Date()) {
